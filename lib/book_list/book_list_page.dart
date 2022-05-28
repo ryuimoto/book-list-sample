10bc4853f043c306100769f2675cbfa485688275
maca <narrow617@gmail.com>
2022-05-28 18:14:04 +0900

    e

--- lib/book_list/book_list_page.dart
+++ lib/book_list/book_list_page.dart
@@ -1,40 +1,39 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:book_list_sample/book_list/book_list_model.dart';
+import 'package:book_list_sample/domain/book.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 class BookListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('本一覧'),
-      ),
-      body: Center(
-        child: StreamBuilder<QuerySnapshot>(
-          stream: _usersStream,
-          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Text("Loading");
-            }
-
-            return ListView(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-                return ListTile(
-                  title: Text(data['title']),
-                  subtitle: Text(data['author']),
-                );
-              }).toList(),
-            );
-          },
+    return ChangeNotifierProvider<BookListModel>(
+      create: (_) => BookListModel()..fetchBookList(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('本一覧'),
         ),
+        body: Center(
+          child: Consumer<BookListModel>(builder: (context,model,child){
+            final List<Book>? books = model.books;
+            if(books == null){
+              return CircularProgressIndicator();
+            }
+            final List<Widget> widgets = books
+                .map(
+                  (book) => ListTile(
+                    title: Text(book.title),
+                    subtitle: Text(book.author),
+                  ),
+            ).toList();
+            return ListView(
+              children: widgets,
+            );
+          }),
+        ),
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
