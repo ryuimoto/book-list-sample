f63a3c6435eb0d1513535673f41cb3b9963ef183
maca <narrow617@gmail.com>
2022-05-28 17:17:10 +0900

    e

--- lib/book_list_page.dart
+++ lib/book_list_page.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 
 class BookListPage extends StatelessWidget {
+  final Stream<QuerySnapshot> _usersStream =
+    FirebaseFirestore.instance.collection('books').snapshots();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,8 +13,23 @@ class BookListPage extends StatelessWidget {
         title: Text('本一覧'),
       ),
       body: Center(
-        child: Text(
-          'You have pushed the button this many times',
+        child: StreamBuilder<QuerySnapshot>(
+          stream: _usersStream,
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
+
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(data['title']),
+                  subtitle: Text(data['author']),
+                );
+              }).toList(),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
